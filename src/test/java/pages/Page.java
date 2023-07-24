725f503a77cef3cc315771746bc40857289f2d89
Erol <122645226+erolisldk@users.noreply.github.com>
2023-07-24 22:35:28 +0300

    Update Page.java

--- src/test/java/pages/Page.java
+++ src/test/java/pages/Page.java
@@ -8,7 +8,7 @@ public class Page {
     public Page() {
         PageFactory.initElements(Driver.getDriver(), this);
         // erol branchinde birseyler yazdim
-
+        //yucel birseyler yazdi
     }
 
 
